ba15152ef7f800bcf57790030cd17af018cf25a5
ilyasahmedshaikh <ilyasahmed081@gmail.com>
2019-10-25 11:00:55 -0700

    sale section done with responsive

--- js/custom.js
+++ js/custom.js
@@ -13,6 +13,48 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
 
+// digital-carousel
+$('.digital-carousel').slick({
+  dots: false,
+  prevArrow: false,
+  nextArrow: false,
+  autoplay:true,
+  autoplaySpeed: 3000,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: false,
+        arrows: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
 // slider-carousel
 $('.slider-carousel').slick({
   dots: false,
@@ -54,42 +96,3 @@ $('.slider-carousel').slick({
   ]
 });
 
-// digital-carousel
-$('.digital-carousel').slick({
-  dots: false,
-  prevArrow: false,
-  nextArrow: false,
-  infinite: false,
-  speed: 300,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true,
-        arrows: false
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
-});
